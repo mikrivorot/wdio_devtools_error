071946deb3a9dac670f8d775d365fcf721b462ed
mikrivorot <m.i.krivorot@gmail.com>
2021-05-04 16:59:01 +0300

    Add usage of cdp

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,7 +1,32 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
 
-describe('My Login application', () => {
+describe('My Login application', async() => {
+    it('should take JS coverage', async () => {
+        /**
+         * enable necessary domains
+         */
+        await browser.cdp('Profiler', 'enable')
+        await browser.cdp('Debugger', 'enable')
+
+        /**
+         * start test coverage profiler
+         */
+        await browser.cdp('Profiler', 'startPreciseCoverage', {
+            callCount: true,
+            detailed: true
+        })
+
+        await browser.url('http://google.com')
+
+        /**
+         * capture test coverage
+         */
+        const { result } = await browser.cdp('Profiler', 'takePreciseCoverage')
+        const coverage = result.filter((res) => res.url !== '')
+        console.log(coverage)
+    })
+
     it('should login with valid credentials', async () => {
         await LoginPage.open();
 
